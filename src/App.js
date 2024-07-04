33dca7085a365118dc0f0309e26e43dddc5a7ef7
Deepak Yadav <deepak.y@mindfiresolutions.com>
2024-07-04 13:15:04 +0530

    feat:basic layout and functionality for text editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import RichTextEditor from "./components/RichTextEditor.jsx";
+const App = () => {
+  const features = [
+    'bold', 'italic', 'underline', 'orderedList', 'unorderedList',
+    'alignLeft', 'alignCenter', 'alignRight', 'createLink', 'insertImage', 'getHtml', 'getJson'
+  ];
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <RichTextEditor features={features} />
   );
-}
+};
 
 export default App;
